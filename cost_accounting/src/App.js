097d7bb909503028e15ef1e7944a0e354cbcd3a7
Serg Mark <sgcont@gmail.com>
2022-11-03 12:22:48 +0300

    Created the first custom component

--- cost_accounting/src/App.js
+++ cost_accounting/src/App.js
@@ -1,7 +1,10 @@
+import CostItem from './components/CostItem'
+
 function App() {
   return (
     <div>
       <h1>React!</h1>
+      <CostItem></CostItem>
     </div>
   )
 }
---
+++ cost_accounting/src/components/CostItem.js
@@ -0,0 +1,13 @@
+function CostItem() {
+  return (
+    <div>
+      <div>октябрь 2022 09</div>
+      <div>
+        <h2>Микроволновка</h2>
+        <div>$240.55</div>
+      </div>
+    </div>
+  )
+}
+
+export default CostItem
