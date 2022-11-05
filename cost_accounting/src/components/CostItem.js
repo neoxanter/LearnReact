ed1e088d60b56f1d2d98e38eb3816e1819fba092
Serg Mark <sgcont@gmail.com>
2022-11-05 11:27:46 +0300

    Changed the text in CostItem.js

--- cost_accounting/src/components/CostItem.js
+++ cost_accounting/src/components/CostItem.js
@@ -3,9 +3,9 @@ import './CostItem.css'
 function CostItem() {
   return (
     <div className="cost-item">
-      <div>октябрь 2022 09</div>
+      <div>October 2022 09</div>
       <div className="cost-item__description">
-        <h2>Микроволновка</h2>
+        <h2>Microwave</h2>
         <div className="cost-item__price">$240.55</div>
       </div>
     </div>
