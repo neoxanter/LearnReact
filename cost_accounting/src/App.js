9cb15535a8bc6068811722e4110d7818049b1d5c
Serg Mark <sgcont@gmail.com>
2022-11-05 12:17:05 +0300

    Added an array of values and several components

--- cost_accounting/src/App.js
+++ cost_accounting/src/App.js
@@ -1,9 +1,30 @@
 import CostItem from './components/CostItem'
 
 function App() {
+
+const costs = [
+  {
+    date: new Date(2022, 3, 09),
+    description: "Microwave",
+    amount: 240.55
+  }
+  {
+    date: new Date(2022, 3, 09),
+    description: "Television",
+    amount: 325.15
+  }
+  {
+    date: new Date(2022, 3, 09),
+    description: "Steamer",
+    amount: 158.60
+  }
+]
+
   return (
     <div>
       <h1>React!</h1>
+      <CostItem></CostItem>
+      <CostItem></CostItem>
       <CostItem></CostItem>
     </div>
   )
