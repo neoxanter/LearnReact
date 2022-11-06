2e4fa96749dddc7c4a1d639373a7fc88e813a35c
Serg Mark <sgcont@gmail.com>
2022-11-06 12:43:07 +0300

    Redesigned part of the App.js to the CostView component and added the style to css

--- cost_accounting/src/App.js
+++ cost_accounting/src/App.js
@@ -1,42 +1,9 @@
-import CostItem from './components/CostItem'
+import CostView from './components/CostView'
 
 function App() {
-  const costs = [
-    {
-      date: new Date(2022, 3, 11),
-      description: 'Microwave',
-      amount: 240.55,
-    },
-    {
-      date: new Date(2022, 4, 23),
-      description: 'Television',
-      amount: 325.15,
-    },
-    {
-      date: new Date(2022, 6, 18),
-      description: 'Steamer',
-      amount: 158.61,
-    },
-  ]
-
   return (
     <div>
-      <h1>React!</h1>
-      <CostItem
-        date={costs[0].date}
-        description={costs[0].description}
-        amount={costs[0].amount}
-      />
-      <CostItem
-        date={costs[1].date}
-        description={costs[1].description}
-        amount={costs[1].amount}
-      />
-      <CostItem
-        date={costs[2].date}
-        description={costs[2].description}
-        amount={costs[2].amount}
-      />
+      <CostView />
     </div>
   )
 }
---
+++ cost_accounting/src/components/CostView.js
@@ -0,0 +1,45 @@
+import './CostView.css'
+import CostItem from './CostItem'
+
+function CostView(props) {
+  const costs = [
+    {
+      date: new Date(2022, 3, 11),
+      description: 'Microwave',
+      amount: 240.55,
+    },
+    {
+      date: new Date(2022, 4, 23),
+      description: 'Television',
+      amount: 325.15,
+    },
+    {
+      date: new Date(2022, 6, 18),
+      description: 'Steamer',
+      amount: 158.61,
+    },
+  ]
+
+  return (
+    <div className="costs">
+      <h1>React!</h1>
+      <CostItem
+        date={costs[0].date}
+        description={costs[0].description}
+        amount={costs[0].amount}
+      />
+      <CostItem
+        date={costs[1].date}
+        description={costs[1].description}
+        amount={costs[1].amount}
+      />
+      <CostItem
+        date={costs[2].date}
+        description={costs[2].description}
+        amount={costs[2].amount}
+      />
+    </div>
+  )
+}
+
+export default CostView
