6a132320574c040d811de42f185f77a36425fc4d
Serg Mark <sgcont@gmail.com>
2022-11-06 12:13:29 +0300

    Added a file CostDate.css to stylize dates

--- cost_accounting/src/components/CostDate.js
+++ cost_accounting/src/components/CostDate.js
@@ -1,13 +1,15 @@
+import './CostDate.css'
+
 function CostDate(props) {
   const month = props.date.toLocaleString('en-GB', { month: 'long' })
   const year = props.date.getFullYear()
   const day = props.date.toLocaleString('en-GB', { day: '2-digit' })
 
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="cost-date">
+      <div className="cost-date__month">{month}</div>
+      <div className="cost-date__year">{year}</div>
+      <div className="cost-date__day">{day}</div>
     </div>
   )
 }
