1ee043c70b6d661d3c499d264b8ecf89ffd75a2a
neoxanter <sgcont@gmail.com>
2023-06-06 13:08:08 +0300

    Created a form for entering expenses

--- cost_accounting/src/components/NewCost/CostForm.js
+++ cost_accounting/src/components/NewCost/CostForm.js
@@ -1,8 +1,25 @@
 const CostForm = () => {
     return (
-        <div>
-            <form></form>
-        </div>
+        <form>
+            <div className="new-cost__controls">
+                <div className="new-cost__control">
+                    <label>Name</label>
+                    <input type="text" />
+                </div>
+            </div>
+            <div className="new-cost__controls">
+                <div className="new-cost__control">
+                    <label>Amount</label>
+                    <input type="number" min='0.01' step='0.01' />
+                </div>
+            </div>
+            <div className="new-cost__controls">
+                <div className="new-cost__control">
+                    <label>Date</label>
+                    <input type="date" min='2020-01-01' step='2022-12-31' />
+                </div>
+            </div>
+        </form>
     )
 }
 
