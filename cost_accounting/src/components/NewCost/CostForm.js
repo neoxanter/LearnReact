86dec23e0b2b2f0a0b998f7843300739ba8f1b0f
neoxanter <sgcont@gmail.com>
2023-06-06 13:10:17 +0300

    bug fix - removed unnecessary className

--- cost_accounting/src/components/NewCost/CostForm.js
+++ cost_accounting/src/components/NewCost/CostForm.js
@@ -6,14 +6,10 @@ const CostForm = () => {
                     <label>Name</label>
                     <input type="text" />
                 </div>
-            </div>
-            <div className="new-cost__controls">
                 <div className="new-cost__control">
                     <label>Amount</label>
                     <input type="number" min='0.01' step='0.01' />
                 </div>
-            </div>
-            <div className="new-cost__controls">
                 <div className="new-cost__control">
                     <label>Date</label>
                     <input type="date" min='2020-01-01' step='2022-12-31' />
