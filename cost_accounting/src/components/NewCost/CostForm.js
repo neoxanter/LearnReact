4777204ba6efe0fc38f5c0b04af37abf589adc7d
neoxanter <sgcont@gmail.com>
2023-06-07 09:52:30 +0300

    Added useState for all inputs in the CostForm component to save values via event.target.value

--- cost_accounting/src/components/NewCost/CostForm.js
+++ cost_accounting/src/components/NewCost/CostForm.js
@@ -1,6 +1,20 @@
+import React, {useState} from "react"
 import './CostForms.css'
+
 const CostForm = () => {
-    const nameChangeHandler = () => {}
+    const [name, setName] = useState('')
+    const [amount, setAmount] = useState('')
+    const [date, setDate] = useState('')
+    const nameChangeHandler = (event) => {
+        setName(event.target.value) //используем здесь state для сохранения значения
+    }
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value)
+    }
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value)
+    }
+
     return (
         <form>
             <div className="new-cost__controls">
@@ -10,11 +24,11 @@ const CostForm = () => {
                 </div>
                 <div className="new-cost__control">
                     <label>Amount</label>
-                    <input type="number" min='0.01' step='0.01' />
+                    <input type="number" onChange={amountChangeHandler} min='0.01' step='0.01'/>
                 </div>
                 <div className="new-cost__control">
                     <label>Date</label>
-                    <input type="date" min='2020-01-01' step='2022-12-31' />
+                    <input type="date" onChange={dateChangeHandler} min='2020-01-01' step='2022-12-31'/>
                 </div>
                 <div>
                     <div className="new-cost__actions">
