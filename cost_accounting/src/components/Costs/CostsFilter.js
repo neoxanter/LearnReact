9cd1a443a6db00f8bef9d0c489698d615757968b
neoxanter <sgcont@gmail.com>
2023-06-08 09:41:34 +0300

    Created a CostsFilter component for selecting by year

---
+++ cost_accounting/src/components/Costs/CostsFilter.js
@@ -0,0 +1,15 @@
+const CostsFilter = () => {
+    return (
+        <div>
+            <div>
+                <label>Filter by year</label>
+                <select>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                </select>
+            </div>
+        </div>
+    )
+}
