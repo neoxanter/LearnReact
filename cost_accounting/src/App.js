c7352f9d69eab57cdbdd734f5aab2e00e0cf1000
neoxanter <sgcont@gmail.com>
2023-06-09 11:13:27 +0300

    Added update of the 'costs' array when entering data into the form fields of the CostForm component using useState

--- cost_accounting/src/App.js
+++ cost_accounting/src/App.js
@@ -1,38 +1,40 @@
 import NewCost from "./components/NewCost/NewCost";
 import CostView from './components/Costs/CostView'
+import React, {useState} from "react";
 
+const INIT_COSTS = [
+  {
+    id: 'c1',
+    date: new Date(2022, 3, 11),
+    description: 'Microwave',
+    amount: 240.55,
+  },
+  {
+    id: 'c2',
+    date: new Date(2022, 4, 23),
+    description: 'Television',
+    amount: 325.15,
+  },
+  {
+    id: 'c3',
+    date: new Date(2022, 6, 18),
+    description: 'Steamer',
+    amount: 158.61,
+  },
+]
 const App = () => {
+  const [costs, setCosts] = useState(INIT_COSTS)
 
-    const costs = [
-        {
-            id: 'c1',
-            date: new Date(2022, 3, 11),
-            description: 'Microwave',
-            amount: 240.55,
-        },
-        {
-            id: 'c2',
-            date: new Date(2022, 4, 23),
-            description: 'Television',
-            amount: 325.15,
-        },
-        {
-            id: 'c3',
-            date: new Date(2022, 6, 18),
-            description: 'Steamer',
-            amount: 158.61,
-        },
-    ]
-    const addCostHandler = (cost) => {
+  const addCostHandler = (cost) => {
+    setCosts([cost, ...costs])
+  }
 
-    }
-
-    return (
-        <div>
-            <NewCost onAddCost={addCostHandler}/>
-            <CostView costs={costs}/>
-        </div>
-    )
+  return (
+      <div>
+        <NewCost onAddCost={addCostHandler}/>
+        <CostView costs={costs}/>
+      </div>
+  )
 }
 
 export default App
