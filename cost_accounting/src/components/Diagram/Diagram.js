cf620f6f8dbb0cb1abeb406d06934f6beb29a177
neoxanter <sgcont@gmail.com>
2023-06-13 10:00:05 +0300

    Added logic to get maximum expenses by month

--- cost_accounting/src/components/Diagram/Diagram.js
+++ cost_accounting/src/components/Diagram/Diagram.js
@@ -1,13 +1,18 @@
 import DiagramBar from "./DiagramBar";
 
 const Diagram = (props) => {
+
+  const dataSetsValues = props.dataSets.map(dataSet => dataSet.value)
+
+  const maxMonthCosts = Math.max(...dataSetsValues)
+
   return (
       <div>
         {props.dataSets.map((dataSet) => (
             <DiagramBar
                 key={dataSet.label}
                 label={dataSet.label}
-                maxValue={null}
+                maxValue={maxMonthCosts}
                 value={dataSet.value}/>
         ))}
       </div>
